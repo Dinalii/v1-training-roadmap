f28571745d7b34469786e54ba8c1becb4cb5c00f
Dinali Indeewari <65953217+Dinalii@users.noreply.github.com>
2024-03-12 14:56:33 +0530

    SCSS Basics

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './app.css';
+import './index.scss';
+import './app.scss';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
